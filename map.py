24deeb5143c2c44dbc5c644da263b9b8f3d98d88
RyanPark0128 <vazywon@hotmail.com>
2022-05-18 13:37:00 -0700

    feat: add infomation about volcanoes, and implement function to determine color of each flag depending on elevations of each volcano

--- map.py
+++ map.py
@@ -4,14 +4,33 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+Volcano name: <a href="https://www.google.com/search?q=Mount+%s" target="_blank">%s</a>
+Elevation: %s m
+"""
+
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 map = folium.Map(location=[49.21, -122.943],
                  zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="My Volcano Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(
-        location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color="green")))
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(
+        iframe), icon=folium.Icon(color=color_producer(el))))
 
 
 map.add_child(fg)
